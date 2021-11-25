f4f7fabcd30199004a79c0c4bb0123f8400ab4fe
solenedel <solene98@student.ubc.ca>
2021-11-24 20:31:16 -0800

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ const Header = ({className}) => {
   return ( 
     <header className={className}>
         <h1>Solène Delumeau</h1>
-        <h3>Full stack developer</h3>
+        <h3>full stack developer.</h3>
+        <h3>France <i class="fas fa-plane" /> Japan <i class="fas fa-plane" /> China <i class="fas fa-plane" /> Singapore <i class="fas fa-plane" /> Canada</h3>
+        <h3><i class="fas fa-map-marker-alt" /> Currently: Greater Vancouver, BC</h3>
     </header>
    );
 }
--- src/components/styled/GlobalStyles.style.js
+++ src/components/styled/GlobalStyles.style.js
@@ -9,9 +9,17 @@ export const GlobalStyles = createGlobalStyle`
     /* background-color: black; */
   }
 
-/* 
-  h1 {
-    margin-top: 50;
-  } */
+    body {
+      background-color: #35B6FF;
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 2 1'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%2335B6FF'/%3E%3Cstop offset='1' stop-color='%236E48FF'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='0' x2='0' y2='1' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23C7A1FF' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23C7A1FF' stop-opacity='1'/%3E%3C/linearGradient%3E%3ClinearGradient id='c' gradientUnits='userSpaceOnUse' x1='0' y1='0' x2='2' y2='2' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23C7A1FF' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23C7A1FF' stop-opacity='1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect x='0' y='0' fill='url(%23a)' width='2' height='1'/%3E%3Cg fill-opacity='0.59'%3E%3Cpolygon fill='url(%23b)' points='0 1 0 0 2 0'/%3E%3Cpolygon fill='url(%23c)' points='2 1 2 0 0 0'/%3E%3C/g%3E%3C/svg%3E");
+      background-attachment: fixed;
+      background-size: cover;
+      
+    }
+
+  p, h1, h2, h3, h4, h5 {
+    font-family: 'Ubuntu Mono', monospace;
+    margin: 15px 0 15px 0;
+  }
 
 `;
