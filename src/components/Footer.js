52c637fa61ff475f0e484bc0483d8ffec564944e
solenedel <solene98@student.ubc.ca>
2021-11-25 20:20:27 -0800

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,20 @@ const Footer = ({className}) => {
     <footer className={className}>
       <h2>Contact me</h2>
       <div className="contact">
-        {/* <div className="contact-method">
-          <i class="far fa-envelope" />
-          <p>solenedelumeau@gmail.com</p>
-        </div> */}
         <div className="contact-method">
-          <i class="fab fa-linkedin" />
+        <a href="https://www.linkedin.com/in/solene-delumeau" target="_blank" rel="noreferrer"><i class="fab fa-linkedin" /></a>
         </div>
         <div className="contact-method">
-          <i class="fab fa-github" />
+          <a href="https://github.com/solenedel" target="_blank" rel="noreferrer"><i class="fab fa-github" /></a>
         </div>
+      </div>
+      <div className="email">
+        <p>solenedelumeau@gmail.com</p>
+      </div>
+
+      <div className="credits">
+        <a href="https://fontawesome.com/">Icons by Font Awesome. </a>
+        <a href="https://icons8.com/icon/36440/postgresql">PostgreSQL icon by Icons8.</a> 
       </div>
     </footer>
    );
--- src/components/styled/Footer.style.js
+++ src/components/styled/Footer.style.js
@@ -7,7 +7,6 @@ const StyledFooter = styled(Footer)`
   width: 85%;
   align-self: center;
   padding-top: 30px;
-  margin-bottom: 200px;
   border-top: solid black 3px;
   display: flex;
   flex-direction: column;
@@ -16,17 +15,50 @@ const StyledFooter = styled(Footer)`
   h2 {
     font-family: 'PT Mono', monospace;
     font-size: 35px;
+    margin-bottom: 50px;
   }
 
   .contact {
     display: flex;
-
-    & i {
-      font-size: 50px;
-      margin: 0 20px 0 20px;
-    }
+    align-items: baseline;
   }
 
+  i {
+      font-size: 55px;
+      margin: 0 30px 20px 30px;
+    }
+
+    a, a:visited {
+      color: black;
+      transition: color 0.5s;
+
+      &:hover {
+        color: #C9F799;
+        transition: color 0.5s;
+      }
+    }
+
+    .email {
+      margin-bottom: 30px;
+
+      & p {
+        font-size: 30px;
+      }
+    }
+
+    .credits {
+      height: 50px;
+      padding: 18px 0 0 20px;
+      background: rgba(0,0,0,0.7);
+      width: 100vw;
+      color: white;
+
+      a, a:hover, a:visited {
+        color: white;
+        opacity: 0.75;
+        text-decoration: none;
+      }
+    }
 `;
 
 export default StyledFooter;
