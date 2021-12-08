67b778ade5d203e9285cf4215f1762ccbfe6ba23
solenedel <solene98@student.ubc.ca>
2021-12-08 15:26:51 -0800

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from "react";
-import pinturaOne from "../images/pinturaOne.png";
+import pintura from "../images/pintura.png";
 import greengrocer from "../images/greengrocer.png";
+import todaylist from "../images/todaylist.png";
+
 
 const Projects = ({className}) => {
   return ( 
@@ -8,8 +10,9 @@ const Projects = ({className}) => {
         <h2>My projects</h2>
         <p>Click to view the deployed project.</p>
         <div className="projects">
-            <a href="https://pintura-mintbean.herokuapp.com/" target="_blank" rel="noreferrer" className="project" id="one"><img src={pinturaOne} alt="Pintura"></img></a>
+            <a href="https://pintura-mintbean.herokuapp.com/" target="_blank" rel="noreferrer" className="project" id="one"><img src={pintura} alt="Pintura"></img></a>
             <a href="https://www.greengrocer.me/" target="_blank" rel="noreferrer" className="project" ><img src={greengrocer} alt="GreenGrocer"></img></a>
+             <a href="https://www.https://solenedel.github.io/TodayList/" target="_blank" rel="noreferrer" className="project" ><img src={todaylist} alt="TodayList"></img></a>
         </div>
       </div>
    );
