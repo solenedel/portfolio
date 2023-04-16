13a727b0a83a573170ec4a35932b4e8f128d1472
Solène <81546747+solenedel@users.noreply.github.com>
2023-04-15 20:27:56 -0700

    add og image

--- src/App.js
+++ src/App.js
@@ -8,27 +8,7 @@ import StyledTools from "./components/styled/Tools.style";
 function App() {
   return (
     <div className="App">
-      <head>
-        {/* <meta
-            property="og:title"
-            content={'UnCrypt – Navigate the Crypto Market'}
-            key="ogtitle"
-          />
-          <meta property="og:type" content="website" key="ogtype" />
-          <meta
-            property="og:description"
-            content={
-              'Track crypto wallet holdings, learn about coins, and more.'
-            }
-            key="ogdesc"
-          /> */}
-          <meta
-            property="og:image"
-            content={'https://github.io/solenedel/portfolio/images/OG_image.png'}
-            key="ogimage"
-          />
-          <meta property="og:image:width" content="200" />
-          <meta property="og:image:height" content="100" /></head>
+      
       <GlobalStyles />
     
         <StyledHeader />
--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <head>
+        {/* <meta
+            property="og:title"
+            content={'UnCrypt – Navigate the Crypto Market'}
+            key="ogtitle"
+          />
+          <meta property="og:type" content="website" key="ogtype" />
+          <meta
+            property="og:description"
+            content={
+              'Track crypto wallet holdings, learn about coins, and more.'
+            }
+            key="ogdesc"
+          /> */}
+          <meta
+            property="og:image"
+            content={'https://github.io/solenedel/portfolio/images/OG_image.png'}
+            key="ogimage"
+          />
+          <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="100" />
+      </head>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
