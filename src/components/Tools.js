8014765302997755591af0a9af0f4834a4362849
Solène <81546747+solenedel@users.noreply.github.com>
2023-04-27 20:05:41 -0700

    add sample pic

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -7,6 +7,7 @@ import sample2 from "../images/art/sample2.png"
 import sample3 from "../images/art/sample3.png"
 import sample4 from "../images/art/sample4.png"
 import sample5 from "../images/art/sample5.png"
+import sample6 from "../images/art/sample6.png"
 
 
 const Tools = ({className}) => {
@@ -19,7 +20,8 @@ const Tools = ({className}) => {
         <img className="art-sample" src={sample2} alt="art sample 2" height="200" />
         <img className="art-sample" src={sample3} alt="art sample 3" height="200" />
         <img className="art-sample" src={sample4} alt="art sample 4" height="200" />
-         <img className="art-sample" src={sample5} alt="art sample 5" height="200" />
+        <img className="art-sample" src={sample5} alt="art sample 5" height="200" />
+              <img className="art-sample" src={sample6} alt="art sample 6" height="200" />
       </div>
       
         <img className="projectsTitle" src="https://see.fontimg.com/api/renderfont4/wy99/eyJyIjoiZnMiLCJoIjoxMDAsInciOjIwMDAsImZzIjo1MCwiZmdjIjoiI0ZGRkZGRiIsImJnYyI6IiNGRkZERkQiLCJ0IjoxfQ/bXkgdG9vbHM/ferrum.png" alt="Final Fantasy fonts" />
